29e8377fd0bab5366b868649587bb0d9707cb64c
ohsemdomain <ohsemdomain@gmail.com>
2025-06-10 12:45:02 +0800

    Update project structure by replacing Vite logo with a custom favicon, refactoring index.html for improved styling, and integrating Tailwind CSS. Add new dependencies for Tailwind and Lucide React, while removing unused files and updating TypeScript configurations for better compatibility.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Waves } from 'lucide-react'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<div>
+				<div className="mr-6 flex items-center space-x-2">
+					<Waves className="h-6 w-6 text-primary" />
+					<span className="hidden font-bold sm:inline-block font-headline">
+						SurfEdge
+					</span>
+				</div>
+			</div>
+			<h1>Vite + React Items Management</h1>
+			<p className="font-geist">1234567890</p>
+			<div className="card">
+				<button type="button" onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
+		</>
+	)
 }
 
 export default App
