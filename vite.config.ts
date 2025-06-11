5e302b329c5801937c751563f35f128e0dc5ffed
ohsemdomain <ohsemdomain@gmail.com>
2025-06-11 09:14:43 +0800

    Update dependencies in package.json, add wrangler.toml to .gitignore, and configure Vite to use Cloudflare plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { cloudflare } from "@cloudflare/vite-plugin"
 import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), cloudflare()],
   server: {
     port: 5009,
   },
